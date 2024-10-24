187ea0449abb2cb9a28d22e1418f28c27c0d51ec
tarunkanagala123 <tarun.kanagala@zemosolabs.com>
2024-10-24 15:06:36 +0530

    Added basic project files

---
+++ front_end/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './App.css';
+import Homepage from './Components/Homepage';
+
+function App() {
+  return (
+   <>
+   <Homepage></Homepage>
+   </>
+  );
+}
+
+export default App;
---
+++ front_end/src/Components/Homepage.tsx
@@ -0,0 +1,9 @@
+import { Typography } from "@mui/material";
+const Homepage=()=>{
+    return(
+        <>
+        <Typography>Welcome to Homepage</Typography>
+        </>
+    )
+}
+export default Homepage;
